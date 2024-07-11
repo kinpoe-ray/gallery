2cbdb38fdc7f506f4599a6437d43f583c8c3b272
Kinpoe.Ray <149134281+kinpoe-ray@users.noreply.github.com>
2024-07-11 15:53:14 +0800

    Update _document.tsx
    change description

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -8,19 +8,19 @@ class MyDocument extends Document {
           <link rel="icon" href="/favicon.ico" />
           <meta
             name="description"
-            content="See pictures from Next.js Conf and the After Party."
+            content="Kinpoe.Ray's Gallery."
           />
           <meta property="og:site_name" content="nextjsconf-pics.vercel.app" />
           <meta
             property="og:description"
-            content="See pictures from Next.js Conf and the After Party."
+            content="Kinpoe.Ray's Gallery."
           />
-          <meta property="og:title" content="Next.js Conf 2022 Pictures" />
+          <meta property="og:title" content="Kinpoe.Ray's Gallery" />
           <meta name="twitter:card" content="summary_large_image" />
-          <meta name="twitter:title" content="Next.js Conf 2022 Pictures" />
+          <meta name="twitter:title" content="Kinpoe.Ray's Gallery" />
           <meta
             name="twitter:description"
-            content="See pictures from Next.js Conf and the After Party."
+            content="Kinpoe.Ray's Gallery."
           />
         </Head>
         <body className="bg-black antialiased">
